464f1b11574424013df199a397a062679660b780
Emily Gosti <egosti@berkeley.edu>
2019-11-12 15:54:10 -0800

    created jank navbar

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <img src="" />
+      <nav>
+        <ul>
+          <li>
+            <a href="">about</a>
+          </li>
+          <li>
+            <a href="">services</a>
+          </li>
+          <li>
+            <a href="">team</a>
+          </li>
+          <li>
+            <a href="">articles</a>
+          </li>
+          <li>
+            <a href="">join</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
