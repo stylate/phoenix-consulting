e2c06482f24659eb279696268f166ef8a45ff553
Emily Gosti <egosti@berkeley.edu>
2019-11-18 14:02:15 -0800

    rough draft of home page

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-title">PHOENIX</header>
+      <div className="Home-visual">
+        <div className="Home-video-overlay"></div>
+        <div className="Home-info">
+          data-driven solutions for biotech, healthcare and pharmaceutical companies
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
