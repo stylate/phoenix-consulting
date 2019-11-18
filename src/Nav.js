1de0c2caa0c87a844710aa65bd30f937ef0ba90b
Emily Gosti <egosti@berkeley.edu>
2019-11-18 13:34:48 -0800

    cleaned up navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import './Nav.css';
+import logo from './phoenixlogo.png';
 
 function Nav() {
   return (
     <div className="Nav">
-      <img src="" />
-      <nav>
-        <ul>
-          <li>
-            <a href="">about</a>
-          </li>
-          <li>
-            <a href="">services</a>
-          </li>
-          <li>
-            <a href="">team</a>
-          </li>
-          <li>
-            <a href="">articles</a>
-          </li>
-          <li>
-            <a href="">join</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="Nav-contents">
+        <img src={logo} className="Nav-logo" alt="phoenix logo" />
+        <nav>
+          <ul>
+            <li>
+              <a href="">about</a>
+            </li>
+            <li>
+              <a href="">services</a>
+            </li>
+            <li>
+              <a href="">team</a>
+            </li>
+            <li>
+              <a href="">articles</a>
+            </li>
+            <li>
+              <a href="">join</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
