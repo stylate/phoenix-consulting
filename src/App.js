e56720c61b59606a05ac25bfc30254ac61caa947
Emily Gosti <egosti@berkeley.edu>
2019-11-19 00:46:57 -0800

    moved to react routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from './Nav.js';
 import Home from './Home.js';
+import Services from './Services.js';
+import logo from './phoenixlogo.png';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Nav />
-    </div>
+    <Router>
+      <div className="Nav">
+        <div className="Nav-contents">
+          <Link to="/">
+            <img src={logo} className="Nav-logo" alt="phoenix logo" />
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to="">about</Link>
+              </li>
+              <li>
+                <Link to="/services">services</Link>
+              </li>
+              <li>
+                <Link to="">team</Link>
+              </li>
+              <li>
+                <Link to="">articles</Link>
+              </li>
+              <li>
+                <Link to="">join</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/services">
+          <Services />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
