3762dfc062aeccf7b2e1c7809ee382f18ca9407d
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-03 15:20:25 -0800

    fix block component and styling

--- src/components/SingleBlockText.js
+++ src/components/SingleBlockText.js
@@ -3,10 +3,14 @@ import classNames from 'classnames';
 import '../layout/SingleBlockText.css';
 
 const SingleBlockText = (props) => {
+  const { header, body } = props;
+  console.log("body: ", body);
   return (
     <div className={classNames(props.className, "SingleBlockText")}>
-      <div className="SingleBlockText__header">{props.header}</div>
-      <div className="SingleBlockText__body">{props.body}</div>
+      <div className="SingleBlockText__header">{header}</div>
+      <div className="SingleBlockText__body">{body.map((paragraph) => {
+        return <p>{paragraph}</p>
+      })}</div>
     </div>
   )
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -6,32 +6,35 @@ import '../layout/About.css'
 // replace texts later with imported json
 // construct new component with hover/box shadows + text for current sponsors
 
-const About = () => (
-    <div>
-        <header>ABOUT</header>
-        <BlockTexts
-            className="mt"
-            texts={[
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-            ]}
-        />
-        <br/><br/><br/>
-        <header>SPONSORS</header>
-        <BlockTexts
-            className="mt"
-            texts={[
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-                {"header": "my header", "body": "my body"},
-            ]}
-        />
-        <header>CURRENT SPONSORS</header>
-    </div>
-);
+const About = () => {
+    const aboutText = [
+        {"header": "Who We Are", "body": ["Founded in 2016, Phoenix Consulting Group has quickly emerged as a trusted partner for healthcare firms large and small. What gives our consultancy strength is the expertise of our consultants: Phoenix members are Berkeley’s best undergraduate entrepreneurs, researchers, and computer scientists. Simultaneously, we draw these consultants because of the strength of our training: Phoenix alumni graduate with unparalleled experience and a powerful network."]},
+        {"header": "my header", "body": ["my body"]},
+        {"header": "my header", "body": ["my body"]},
+        {"header": "my header", "body": ["my body"]},
+    ];
+    return (
+        <div>
+            <header>ABOUT</header>
+            <BlockTexts
+                className="mt"
+                texts={aboutText}
+            />
+            <br/><br/><br/>
+            <header>SPONSORS</header>
+            <BlockTexts
+                className="mt"
+                texts={[
+                    {"header": "my header", "body": ["my body"]},
+                    {"header": "my header", "body": ["my body"]},
+                    {"header": "my header", "body": ["my body"]},
+                    {"header": "my header", "body": ["my body"]},
+                ]}
+            />
+            <header>CURRENT SPONSORS</header>
+        </div>
+    );
+};
 
 
 export default About;
