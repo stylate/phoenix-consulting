ac65730b98ce89c382743008a6aea07c689c5c88
Courtnie Chan <courtnie@berkeley.edu>
2019-12-03 17:29:16 -0800

    loaded and rotated headshots

--- src/routes/team/MemberImg.js
+++ src/routes/team/MemberImg.js
@@ -2,35 +2,27 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import headshot from '../../assets/headshots/anmol.jpg';
 import '../../layout/MemberImg.css';
-
-
-// const headshotSize = {
-//   height: "10em"
-// }
 
 class MemberImg extends React.Component {
   render() {
-    // const headshot = this.props.obj.imgPath;
-    // console.log(this.props.obj.imgPath);
     return (
       <div>
-        <div class="content">
-            <div class="content-overlay"></div>
-            <img class="content-image" src={headshot} alt={this.props.obj.name}/>
+        <div className="content">
+            <div className="content-overlay"></div>
+            <img className="content-image" src={require(`../../assets/headshots/${this.props.obj.imgPath}`)} alt={this.props.obj.name}/>
 
-            <div class="content-details fadeIn-bottom">
-              <p class="major">{this.props.obj.major}</p>
-              <div class="social-icons">
+            <div className="content-details fadeIn-bottom">
+              <p className="major">{this.props.obj.major}</p>
+              <div className="social-icons">
                 <a href={'mailto:'+this.props.obj.email} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a>
                 <a href={this.props.obj.linkedin} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /></a>
               </div>
             </div>
         </div>
-        <div class="description">
-          <h3 class="name">{this.props.obj.name}</h3>
-          <p class="position">{this.props.obj.position}</p>
+        <div className="description">
+          <h3 className="name">{this.props.obj.name}</h3>
+          <p className="position">{this.props.obj.position}</p>
         </div>
       </div>
     )
