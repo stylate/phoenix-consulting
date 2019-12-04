a21c5c26733c374d19affdb7b1e7be3427615c88
Anand Chandra <anandchandra50@gmail.com>
2019-12-03 16:33:15 -0800

    fixed images

--- src/components/SingleBlockText.js
+++ src/components/SingleBlockText.js
@@ -8,13 +8,13 @@ const SingleBlockText = (props) => {
     <div className={classNames(props.className, "SingleBlockText")}>
       <div className="SingleBlockText__header">{header}</div>
       {image
-        ? <div>
-            <div className="SingleBlockText__body">
+        ? <div className="SingleBlockText__container">
+            <div className="SingleBlockText__body SingleBlockText__body-mr">
               {body.map((paragraph) => {
                 return <p>{paragraph}</p>
               })}
-            </div> 
-            <img src={image[Object.keys(image)[0]]} alt="image" />
+            </div>
+            <img className="SingleBlockText__image" src={image[Object.keys(image)[0]]} alt="image" />
           </div>
         : <div className="SingleBlockText__body">
             {body.map((paragraph) => {
